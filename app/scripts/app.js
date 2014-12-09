f0f5ecc5202b52422d28e291bbef3b29d189a3d5
Kayoungbum <korea0333@gmail.com>
2014-12-09 16:49:18 +0900

    1. html / css 추가 작업 진행. 2. popup쪽 화면 생성. 3. 없는 화면 임시 구성하여 연결.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,10 +27,6 @@ angular
         templateUrl: 'views/main/search/search.html',
         controller: 'AboutCtrl'
       })
-      .when('/create-card', {
-        templateUrl: 'views/popup/create-card/create-card.html',
-        controller: 'AboutCtrl'
-      })
       .when('/activity', {
         templateUrl: 'views/main/activity/activity.html',
         controller: 'AboutCtrl'
@@ -47,20 +43,52 @@ angular
         templateUrl: 'views/popup/auth/signup/signup.html',
         controller: 'AboutCtrl'
       })
+      .when('/create-card', {
+        templateUrl: 'views/popup/create-card/create-card.html',
+        controller: 'AboutCtrl'
+      })
       .when('/create-group', {
         templateUrl: 'views/popup/create-group/create-group.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/edit-group', {
+        templateUrl: 'views/popup/edit-group/edit-group.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/group', {
+        templateUrl: 'views/popup/group/group.html',
         controller: 'AboutCtrl'
       })
       .when('/invite-member', {
         templateUrl: 'views/popup/invite-member/invite-member.html',
         controller: 'AboutCtrl'
       })
-      .when('/select-group', {
-        templateUrl: 'views/popup/select-group/select-group.html',
+      .when('/language', {
+        templateUrl: 'views/popup/language/language.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/member', {
+        templateUrl: 'views/popup/member/member.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/password', {
+        templateUrl: 'views/popup/password/password.html',
         controller: 'AboutCtrl'
       })
       .when('/polling', {
         templateUrl: 'views/popup/polling/polling.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/user-profile', {
+        templateUrl: 'views/popup/profile/profile.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/select-group', {
+        templateUrl: 'views/popup/select-group/select-group.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/select-member', {
+        templateUrl: 'views/popup/select-member/select-member.html',
         controller: 'AboutCtrl'
       })
       .otherwise({
